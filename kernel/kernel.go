bfe8f9f1638378fcda823b042344a52cb384217e
nacho-castro <ignaciocastroplanas@gmail.com>
2025-04-11 17:36:05 -0300

    comments

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	
 )
 
 func init() {
@@ -9,5 +12,21 @@ func init() {
 }
 
 func main() {
+	log.Println("Comenzó ejecucion del Kernel")
+
+	//FUNCIONES DE CLIENTE. CONEXION CON OTROS MODULOS:
+	//enviar mensaje
+	//generar y enviar paquete
+	
+
 	fmt.Println("Hello, World!")
+
+	//TODO ver guía de golang en la sección de "Protocolo HTTP".
+
+	
+
+	//FUNCIONES SERVER DE LOS OTROS MODULOS:
+	//mux.HandleFunc("/paquetes", utils.RecibirPaquetes)
+	//mux.HandleFunc("/mensaje", utils.RecibirMensaje)
+
 }
