e251f9a8bb293f07138d1ea3847bcf9e3930084d
josephmansilla <jomansilla@frba.utn.edu.ar>
2025-04-14 14:24:44 -0300

    un comentario mal

--- cpu/globals/globals.go
+++ cpu/globals/globals.go
@@ -1,6 +1,6 @@
 package globals
 
-// No se si es correcto crear una carpeta utils
+// No se si es correcto crear una carpeta globals
 type Config struct {
 	IpSelf     string `json:"ip_self"`
 	PortSelf   int    `json:"port_self"`
