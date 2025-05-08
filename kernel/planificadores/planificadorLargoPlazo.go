f9db5f27dad073912f13bb22f135dfef0189dbfe
nacho-castro <ignaciocastroplanas@gmail.com>
2025-05-07 22:24:15 -0300

    planificador largo

--- kernel/planificadores/planificadorLargoPlazo.go
+++ kernel/planificadores/planificadorLargoPlazo.go
@@ -11,43 +11,10 @@ import (
 //encargado de gestionar las peticiones a la memoria
 //para la creación y eliminación de procesos e hilos.
 
-// CREA PROCESO (NEW) E INICIA (READY)
+// CREA PROCESO (NEW)
 func CrearProceso(fileName string, tamanio int) {
-	logger.Info("Intentando crear el proceso con pseudocódigo: %s y tamaño: %d", fileName, tamanio)
 
-	/*
-		Al llegar un nuevo proceso a esta cola (NEW) y la misma esté vacía
-		se enviará un pedido a Memoria para inicializar el mismo.
-	*/
-
-	/*
-		En cambio, si un proceso llega a esta cola y ya hay otros esperando
-		se debe tener en cuenta el algoritmo definido y verificar si corresponde o no su ingreso.
-	*/
-
-	// Paso 1: Pedirle a memoria que reserve espacio
-	exito, err := comunicacion.SolicitarCreacionEnMemoria(fileName, tamanio)
-	if err != nil {
-		logger.Error("Error al intentar reservar memoria: %v", err)
-		return
-	}
-
-	/*
-		Si la respuesta es negativa (ya que la Memoria no tiene espacio suficiente para inicializarlo)
-		se deberá esperar la finalización de otro proceso para volver a intentar inicializarlo.
-	*/
-
-	if !exito {
-		logger.Info("Memoria rechazó la creación del proceso (no hay espacio suficiente o error interno)")
-		return
-	}
-
-	/*
-		Si la respuesta es positiva,
-		se pasará el proceso al estado READY y se sigue la misma lógica con el proceso que sigue.
-	*/
-
-	// Paso 2: Crear el PCB y encolarlo
+	// Paso 1: Crear el PCB
 	pid := globals.GenerarNuevoPID()
 	pcbNuevo := pcb.PCB{
 		PID: pid,
@@ -56,8 +23,57 @@ func CrearProceso(fileName string, tamanio int) {
 		MT:  make(map[string]int),
 	}
 
-	globals.ColaNuevo.Add(&pcbNuevo)
-	logger.Info("## (<%d>) Se crea el proceso - Estado: NEW", pid)
+	/*
+		Al llegar un nuevo proceso a cola (NEW) y la misma esté vacía
+		se enviará un pedido a Memoria para inicializar el mismo.
+	*/
+
+	if globals.ColaNuevo.IsEmpty() {
+
+		globals.ColaNuevo.Add(&pcbNuevo)
+		logger.Info("## (<%d>) Se crea el proceso - Estado: NEW", pid)
+
+		IniciarProceso(pcbNuevo, fileName, tamanio)
+	} else {
+		/*
+			En cambio, si un proceso llega a esta cola y ya hay otros esperando
+			se debe tener en cuenta el algoritmo definido
+			y verificar si corresponde o no su ingreso.
+		*/
+		globals.ColaNuevo.Add(&pcbNuevo)
+	}
+}
+
+// INICIA (READY)
+func IniciarProceso(pcb pcb.PCB, fileName string, tamanio int) {
+	logger.Info("Intentando crear el proceso con pseudocódigo: %s y tamaño: %d", fileName, tamanio)
+
+	// Paso 1: Pedirle a memoria que reserve espacio
+	exito, err := comunicacion.SolicitarCreacionEnMemoria(fileName, tamanio)
+	if err != nil {
+		logger.Error("Error al intentar reservar memoria: %v", err)
+		return
+	}
+
+	if !exito {
+		logger.Info("Memoria rechazó la creación del proceso (no hay espacio suficiente o error interno)")
+
+		/*
+			Si la respuesta es negativa (ya que la Memoria no tiene espacio suficiente para inicializarlo)
+			se deberá esperar la finalización de otro proceso para volver a intentar inicializarlo.
+		*/
+		return
+
+	}
+
+	/*
+		Si la respuesta es positiva,
+		se pasará el proceso al estado READY y se sigue la misma lógica con el proceso que sigue.
+	*/
+
+	globals.ColaReady.Add(&pcb)
+	globals.ColaNuevo.Remove(&pcb)
+	logger.Info("## (<%d>) Pasa del estado <NEW> al estado <READY>", pcb.PID)
 }
 
 // FIN DE PROCESO (EXIT)
