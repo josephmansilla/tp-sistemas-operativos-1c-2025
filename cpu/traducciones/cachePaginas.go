e352fda24ae6d622d847b881fb23f56aafa2a2e1
SantiagoTorres24 <storresfranco@frba.utn.edu.ar>
2025-06-09 22:30:38 -0300

    Arrancando cacheeeeee

--- cpu/traducciones/cachePaginas.go
+++ cpu/traducciones/cachePaginas.go
@@ -1,18 +1,114 @@
 package traducciones
 
-import "log"
+import (
+	"github.com/sisoputnfrba/tp-golang/cpu/globals"
+	"log"
+	"sync"
+)
 
-func Leer(dirFisica int, tamanio int) (string, error) {
+type EntradaCache struct {
+	Pagina     int
+	Contenido  string
+	Modificado bool
+	Usado      bool
+}
 
-	log.Printf("Direccion leida: %d - Tamanio leido: %d", dirFisica, tamanio)
+type CachePaginas struct {
+	Entradas    []EntradaCache
+	MaxEntradas int
+	Algoritmo   string
+	Puntero     int
+	mutex       sync.Mutex
+}
 
+var cache *CachePaginas
+var max = globals.ClientConfig.CacheEntries
+
+func NuevaCachePaginas() *CachePaginas {
+	if max <= 0 {
+		return nil
+	}
+	return &CachePaginas{
+		Entradas:    make([]EntradaCache, 0, max),
+		MaxEntradas: max,
+		Algoritmo:   globals.ClientConfig.CacheReplacement,
+		Puntero:     0,
+	}
+}
+
+func (c *CachePaginas) Agregar(pagina int, contenido string, modificado bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	nueva := EntradaCache{
+		Pagina:     pagina,
+		Contenido:  contenido,
+		Modificado: modificado,
+		Usado:      true,
+	}
+
+	if len(c.Entradas) < c.MaxEntradas {
+		c.Entradas = append(c.Entradas, nueva)
+	} else {
+		c.reemplazarEntrada(nueva)
+	}
+}
+
+func (c *CachePaginas) Buscar(pagina int) (string, bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	for i := range c.Entradas {
+		if c.Entradas[i].Pagina == pagina {
+			c.Entradas[i].Usado = true
+			log.Printf("PID: %d - CACHE HIT - Página: %d", globals.PIDActual, pagina)
+			return c.Entradas[i].Contenido, true
+		}
+	}
+	log.Printf("PID: %d - CACHE MISS - Página: %d", globals.PIDActual, pagina)
+	return "", false
+}
+
+func (c *CachePaginas) EstaActiva() bool {
+	return c != nil && c.MaxEntradas > 0
+}
+
+func (c *CachePaginas) reemplazarEntrada(nueva EntradaCache) {
+	//TODO
+}
+
+func (c *CachePaginas) MarcarUso(pagina int) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	for i := range c.Entradas {
+		if c.Entradas[i].Pagina == pagina {
+			c.Entradas[i].Usado = true
+			log.Printf("PID: %d - CACHE USO - Página %d marcada con bit de uso en true", globals.PIDActual, pagina)
+			return
+		}
+	}
+}
+
+func LeerEnCache(pagina int, tamanio int) (string, error) {
+	if contenido, ok := cache.Buscar(pagina); ok {
+		cache.MarcarUso(pagina) // actualizás el bit de uso si usás CLOCK
+		// Simulás leer solo una porción si querés
+		return contenido, nil
+	}
 	return "", nil
 }
 
-func Escribir(dirFisica int, datos string) error {
-
-	log.Printf("Se escribio %s en la direccion fisica %d", datos, dirFisica)
-
+func EscribirEnCache(pagina int, datos string) error {
+	for i := range cache.Entradas {
+		if cache.Entradas[i].Pagina == pagina {
+			cache.Entradas[i].Contenido = datos
+			cache.Entradas[i].Modificado = true
+			cache.Entradas[i].Usado = true
+			log.Printf("Se escribio %s en página %d", datos, pagina)
+			return nil
+		}
+	}
 	return nil
 }
 
